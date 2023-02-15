b2f28b99d22680761c57f45a5f6f65e835633404
zip-zoop <Jason Lee>
2023-02-14 18:23:48 -0700

    put a box centered inside for skills

--- pages/index.js
+++ pages/index.js
@@ -148,7 +148,132 @@ const Home = () => (
           My Skills 
         </Heading>
           <Paragraph class="indented" >
-                            
+            <Box
+              borderRadius="sm"
+              mb={1}
+              p={1}
+              textAlign="center"
+              bg={useColorModeValue('whiteAlpha.01', 'whiteAlpha.01')}
+              css={{ backdropFilter: 'blur(10px)' }}
+            >
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                size = 'lg'
+                leftIcon={< SiAutodesk />}
+              >
+                Maya
+              </Button>
+            •
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                size = 'lg'
+                leftIcon={< SiCinema4D />}
+              >
+                Cinema 4d
+              </Button>
+            • 
+
+            <Button
+                variant="ghost"
+                colorScheme="teal"
+                size = 'lg'
+                leftIcon={< SiBlender />}
+              >
+                Blender
+              </Button>
+              
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                size = 'lg'
+                leftIcon={< SiAdobepremierepro />}
+              >
+                Premiere Pro
+              </Button>
+          {/* <Link href="https://adobe.com" target="_blank"> */}
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                size = 'lg'
+                leftIcon={< SiAdobeaftereffects />}
+              >
+                After Effects
+              </Button>
+            {/* </Link> */}
+            • 
+          <Button
+              variant="ghost"
+              colorScheme="teal"
+              size = 'lg'
+              leftIcon={< SiAdobelightroom />}
+            >
+              Lightroom
+            </Button>
+            • 
+          <Button
+              variant="ghost"
+              colorScheme="teal"
+              size = 'lg'
+              leftIcon={< SiAdobephotoshop />}
+            >
+              Photoshop
+            </Button>
+
+            • 
+          <Button
+              variant="ghost"
+              colorScheme="teal"
+              size = 'lg'
+              leftIcon={< SiUnrealengine />}
+            >
+              Unreal Engine 5
+            </Button>
+            • 
+          <Button
+              variant="ghost"
+              colorScheme="teal"
+              size = 'lg'
+              leftIcon={< SiPython />}
+            >
+              Python
+            </Button>
+            • 
+          <Button
+              variant="ghost"
+              colorScheme="teal"
+              size = 'lg'
+              leftIcon={< SiR />}
+            >
+              R
+            </Button>
+            • 
+          <Button
+              variant="ghost"
+              colorScheme="teal"
+              size = 'lg'
+            >
+              RundownCreator
+            </Button>
+            • 
+            <Button
+              variant="ghost"
+              colorScheme="teal"
+              size = 'lg'
+            >
+              Ross Synergy 3 Switchboard
+            </Button>
+            • 
+            <Button
+              variant="ghost"
+              colorScheme="teal"
+              size = 'lg'
+            >
+              Yamaha O2R96
+            </Button>
+            </Box>
+            ohbaby    
               <Button
                 variant="ghost"
                 colorScheme="teal"
